241de33a10712d83c32e5424f9a23fd422d173b6
Andrew Magill <AndyMagill@Gmail.com>
2019-11-10 14:40:23 -0500

    added footer contact form

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 	get_header();
 
 	get_template_part('template-parts/hero');
-
 ?>
 	<section class="middle-nav">
 		<?php magillDev()->render_menus('middle-menu'); ?>
@@ -19,9 +18,21 @@
 	<!-- /main -->
 
 	<section class="contact">
-		<?= do_shortcode('[wpforms id="26" title="false" description="false"]'); ?>
+		<div class="row wrapper">
+			<div class="col email_text">
+				<div class="col_inner">
+					<h2>Interested in working together?</h2>
+					<p>Are you one of those strange people that loves to talk about work?  What a coincidence, me too! I would love to hear from you so lets start a discussion.</p>
+				</div>
+			</div>
+
+			<div class="col">
+				<div class="col_inner">
+					<?= do_shortcode('[wpforms id="26" title="false" description="false"]'); ?>
+				</div>
+			</div>
+		</div>
 	</section>
 	<!-- /contact -->
-
 
 <?php get_footer(); ?>
